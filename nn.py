4dadcaad77e7c7e5e238e4ecb316f43ae776f9d7
Eddy Yeo <eddy.yeo@gmail.com>
2018-11-15 01:17:29 -0500

    Implemented training

--- nn.py
+++ nn.py
@@ -1,16 +1,16 @@
 '''
 TODO:
-- Train it
-- Beam search
 - DSL
+- generate training data
+- train it
+- Beam search
 '''
 
 
 import torch
 import torch.nn as nn
-
-
-MAX_PROGRAM_LENGTH = 10
+import torch.nn.functional as F
+import torch.optim as optim
 
 
 class RobustFill(nn.Module):
@@ -19,16 +19,20 @@ class RobustFill(nn.Module):
             num_tokens,
             string_size,
             program_size,
-            hidden_size,
-            end_of_sequence):
+            program_length):
         super().__init__()
-        self.embedding = nn.Embedding(num_tokens, string_size)
-        self.input_lstm = nn.LSTM(string_size, hidden_size)
-        self.output_lstm = nn.LSTM(string_size, hidden_size)
-        self.program_lstm = nn.LSTM(program_size, hidden_size)
-        self.end_of_sequence = end_of_sequence
+
+        # Add 1 to num_tokens for end-of-sequence token
+        self.embedding = nn.Embedding(num_tokens + 1, string_size)
+        self.end_of_sequence_index = torch.LongTensor([num_tokens])
+
+        self.input_lstm = nn.LSTM(string_size, program_size)
+        self.output_lstm = nn.LSTM(string_size, program_size)
+        self.program_lstm = nn.LSTM(program_size, program_size)
+        self.program_length = program_length
 
     def forward(self, input_sequence, output_sequence):
+        end_of_sequence = self.embedding(self.end_of_sequence_index)
         input_sequence = [
             self.embedding(torch.LongTensor([index]))
             for index in input_sequence
@@ -42,39 +46,58 @@ class RobustFill(nn.Module):
         for c in input_sequence:
             _, hidden = self.input_lstm(c.view(1, 1, -1), hidden)
         _, hidden = self.input_lstm(
-                self.end_of_sequence.view(1, 1, -1),
+                end_of_sequence.view(1, 1, -1),
                 hidden,
         )
 
         for c in output_sequence:
             _, hidden = self.output_lstm(c.view(1, 1, -1), hidden)
         _, hidden = self.output_lstm(
-                self.end_of_sequence.view(1, 1, -1),
+                end_of_sequence.view(1, 1, -1),
                 hidden,
         )
 
         program_sequence = []
-        for _ in range(MAX_PROGRAM_LENGTH):
+        for _ in range(self.program_length):
             # Should the first input be hidden?
             _, hidden = self.program_lstm(hidden[0].view(1, 1, -1), hidden)
-            program_sequence.append(hidden[0])
-            # TODO: Break if hidden == end of sequence
+            program_embedding = F.log_softmax(
+                torch.squeeze(hidden[0], dim=1),
+                dim=1,
+            )
+            program_sequence.append(program_embedding)
 
         return program_sequence
 
 
 def main():
     torch.manual_seed(1337)
+
     robust_fill = RobustFill(
-        num_tokens=3,
-        string_size=3,
+        num_tokens=2,
+        string_size=2,
         program_size=3,
-        hidden_size=3,
-        end_of_sequence=torch.randn(3),
+        program_length=2,
     )
-    input_sequence = [0, 1, 2]
-    output_sequence = [2, 1, 0]
-    print(robust_fill(input_sequence, output_sequence))
+    optimizer = optim.SGD(robust_fill.parameters(), lr=0.001)
+
+    input_sequence = [0, 0, 0]
+    output_sequence = [0, 0, 0]
+
+    for i in range(10000):
+        optimizer.zero_grad()
+
+        program_sequence = robust_fill(input_sequence, output_sequence)
+        loss = F.nll_loss(
+            torch.cat(program_sequence),
+            torch.LongTensor([0, 2]),
+        )
+
+        loss.backward()
+        optimizer.step()
+
+        if i % 1000 == 0:
+            print(loss)
 
 
 if __name__ == '__main__':
