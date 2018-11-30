41859097b67b222780af0304787f518713ea6c29
Eddy Yeo <eddy.yeo@gmail.com>
2018-11-30 00:22:18 -0500

    Implemented sampling

---
+++ sample_example.py
@@ -0,0 +1,69 @@
+from collections import namedtuple
+import logging
+import random
+
+from evaluate import evaluate
+from sample_program import sample_program
+from to_string import to_string
+import operators as op
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+Example = namedtuple(
+    'Example',
+    ['program', 'strings', 'num_discarded_programs'],
+)
+
+
+def sample_string(program, max_characters):
+    num_characters = random.randint(1, max_characters)
+    random_string = ''.join(random.choices(op.CHARACTER, k=num_characters))
+    return random_string
+
+
+def sample_example(
+        *,
+        max_expressions=10,
+        max_characters=100,
+        max_empty_strings=2,
+        num_strings=4,
+        discard_program_num_empty=100,
+        discard_program_num_exceptions=100):
+    num_discarded = 0
+    while True:
+        program = sample_program(max_expressions)
+
+        num_empty, num_exception = 0, 0
+        sampled_strings = []
+
+        while True:
+            string = sample_string(program, max_characters)
+            try:
+                transformed = evaluate(program, string)
+
+                assert isinstance(transformed, str)
+
+                if len(transformed) == 0:
+                    num_empty += 1
+                if num_empty < max_empty_strings:
+                    sampled_strings.append((string, transformed))
+
+            except IndexError:
+                num_exception += 1
+
+            if len(sampled_strings) == num_strings:
+                return Example(program, sampled_strings, num_discarded)
+
+            if (num_empty > discard_program_num_empty
+                    or num_exception > discard_program_num_exceptions):
+                LOGGER.debug('Throwing program away')
+                LOGGER.debug(
+                    'Empty: %s, exception: %s',
+                    num_empty,
+                    num_exception,
+                )
+                LOGGER.debug(to_string(program))
+                num_discarded += 1
+                break
--- test_sample.py
+++ test_sample.py
@@ -1,20 +1,18 @@
 from unittest import TestCase
 
-from evaluate import evaluate
-from sample_program import sample_program
+from sample_example import sample_example
 
 
 class TestSample(TestCase):
-    def test_sample_program_smoke_test(self):
-        success = 0
-        fail = 0
-        for _ in range(1000):
-            program = sample_program(10)
-            try:
-                transformed = evaluate(program, 'a b c d e 1 2 3 4 5')
-                assert isinstance(transformed, str)
-                success += 1
-            except IndexError:
-                # TODO: Remove after implementing string generation
-                fail += 1
-        print('Succeeded: {}, Failed: {}'.format(success, fail))
+    def test_sample_example_smoke_test(self):
+        num_discarded_programs = 0
+        num_tries = 100
+        for _ in range(num_tries):
+            example = sample_example()
+            num_discarded_programs += example.num_discarded_programs
+
+        # Seems to be at between 10-20%
+        print('Number of discarded programs: {}/{}'.format(
+            num_discarded_programs,
+            num_tries,
+        ))
