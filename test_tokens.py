f3758cba4800b16bac90e019a12a95f1367b523a
Eddy Yeo <eddy.yeo@gmail.com>
2018-12-08 23:16:30 -0500

    Added .to_tokens() smoke test

--- test_tokens.py
+++ test_tokens.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from sample import sample_program
 from tokens import build_token_tables
 
 
@@ -10,3 +11,10 @@ class TestTokens(TestCase):
         expected_num_tokens = 1118
         self.assertEqual(expected_num_tokens, len(token_tables.token_op_table))
         self.assertEqual(expected_num_tokens, len(token_tables.op_token_table))
+
+    def test_token_table_coverage_smoke_test(self):
+        token_tables = build_token_tables()
+
+        num_samples = 1000
+        for _ in range(num_samples):
+            sample_program(10).to_tokens(token_tables.op_token_table)
